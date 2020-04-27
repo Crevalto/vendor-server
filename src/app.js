d8ab809e1b02166eb3b1198dc296ccf26aaeea6b
magdhoom <sheikmagdhoom15@gmail.com>
2020-04-27 21:39:21 +0530

    new commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('./db/mongoose')
-const app = express()
+const app = express() // express object called app
 
 app.use(express.json())
 module.exports = app
